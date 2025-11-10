00024c5e647baa6760765be4d419bb9f4c3eb058
trinhltl <lamtrinhh1201@gmail.com>
2025-11-10 12:49:04 +0700

    Update jump input method and add new material files for player character

--- Assets/_GAME/Scripts/PlayerMovement.cs
+++ Assets/_GAME/Scripts/PlayerMovement.cs
@@ -102,7 +102,7 @@ public class PlayerMovement : MonoBehaviour
             _animator.SetFloat(_lastVerticalParam, z);
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
