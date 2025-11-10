05c56d00a993c6b5eac17ce236a26f6539e2a452
trinhltl <lamtrinhh1201@gmail.com>
2025-11-10 08:36:31 +0700

    Refactor project structure and add new skybox materials and scripts

---
+++ Assets/_GAME/Scripts/FreelookCameraController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FreelookCameraController : MonoBehaviour
+{
+    private CinemachineFreeLook _freelookCamera;
+
+    private void Awake()
+    {
+        this._freelookCamera = GetComponent<CinemachineFreeLook>();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        var currentSsene = scene.name;
+        if (currentSsene == "FirstScene")
+        {
+            this._freelookCamera.enabled = false;
+        }
+        else
+        {
+            this._freelookCamera.enabled = true;
+        }
+    }
+}
